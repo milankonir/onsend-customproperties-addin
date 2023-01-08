82dc8350291fc7cc0e28abee1bc246cb814f9055
Milan Konir <milan.konir@outreach.io>
2023-01-08 23:36:07 +0100

    set error info into body

--- src/functions.ts
+++ src/functions.ts
@@ -4,8 +4,13 @@ async function onSend(event: Office.AddinCommands.Event) {
   setTimeout(() => {
     Office.context.mailbox.item.loadCustomPropertiesAsync((result) => {
       if (result.status === Office.AsyncResultStatus.Failed) {
+        const { name, code, message } = result.error ?? {};
+        const failureMessage = `[Error] Name: ${name}, Code: ${code}, Message: ${message}`;
+
         Office.context.mailbox.item.body.setAsync(
-          "Failed to load custom properties :((((",
+          `Failed to load custom properties with following error ;(
+
+${failureMessage}`,
           { coercionType: Office.CoercionType.Text },
           () => {
             event.completed({ allowEvent: false });
